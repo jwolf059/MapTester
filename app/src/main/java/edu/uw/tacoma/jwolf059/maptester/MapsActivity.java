ca02bd24d5c09f9444f25e1ddac5cf045719e6a4
jwolf <jwolf059@uw.edu>
2016-11-16 06:37:08 -0800

    Initial commit

---
+++ app/src/main/java/edu/uw/tacoma/jwolf059/maptester/MapsActivity.java
@@ -0,0 +1,136 @@
+package edu.uw.tacoma.jwolf059.maptester;
+
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.support.v4.app.FragmentActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    public static final String URL = "https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=47.253361, -122.439191&keyword=brewery&name=bar&type=pub&radius=10000&key=AIzaSyDgWayjTjcBeGP1CorVrop9OAqBrnRx1Cc";
+    private GoogleMap mMap;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        LoginTask task = new LoginTask();
+        task.execute(new String[]{URL.toString()});
+
+
+
+    }
+
+
+    /**
+     * Manipulates the map once available.
+     * This callback is triggered when the map is ready to be used.
+     * This is where we can add markers or lines, add listeners or move the camera. In this case,
+     * we just add a marker near Sydney, Australia.
+     * If Google Play services is not installed on the device, the user will be prompted to install
+     * it inside the SupportMapFragment. This method will only be triggered once the user has
+     * installed Google Play services and returned to the app.
+     */
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+
+        // Add a marker in Sydney and move the camera
+        LatLng tacoma = new LatLng(47.253361, -122.43919);
+        mMap.addMarker(new MarkerOptions().position(tacoma).title("Marker in Sydney"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(tacoma));
+    }
+
+    //NEED this
+    private class LoginTask extends AsyncTask<String, Void, String> {
+
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String response = "";
+            HttpURLConnection urlConnection = null;
+            for (String url : urls) {
+                try {
+                    java.net.URL urlObject = new URL(url);
+                    urlConnection = (HttpURLConnection) urlObject.openConnection();
+
+                    InputStream content = urlConnection.getInputStream();
+
+                    BufferedReader buffer = new BufferedReader(new InputStreamReader(content));
+                    String s = "";
+                    while ((s = buffer.readLine()) != null) {
+                        response += s;
+                    }
+
+                } catch (Exception e) {
+                    response = "Unable to Login, Reason: "
+                            + e.getMessage();
+                } finally {
+                    if (urlConnection != null)
+                        urlConnection.disconnect();
+                }
+            }
+            return response;
+        }
+
+
+        /**
+         * It checks to see if there was a problem with the URL(Network) which is when an
+         * exception is caught. It tries to call the parse Method and checks to see if it was successful.
+         * If not, it displays the exception.
+         *
+         * @param result
+         */
+        @Override
+        protected void onPostExecute(String result) {
+
+            try {
+
+                JSONObject jsonObject = new JSONObject(result);
+                JSONArray jArray = jsonObject.getJSONArray("results");
+                Log.i("Json  ", jsonObject.toString());
+                for (int i = 0; i < jArray.length(); i++) {
+                    JSONObject obj = jArray.getJSONObject(i);
+                    Log.i("Name", obj.getString("name"));
+                }
+
+
+
+
+            } catch (JSONException e) {
+                Toast.makeText(getApplicationContext(), "Something wrong with the data" +
+                        e.getMessage(), Toast.LENGTH_LONG).show();
+                Log.e("Wrong Data", e.getMessage());
+            }
+
+        }
+    }
+}
